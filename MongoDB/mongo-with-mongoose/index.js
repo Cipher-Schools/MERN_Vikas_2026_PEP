e04cbcb2f6bd391e0b7d0a8296e13806a7befc8c
vikas-mhjn <vk5008953@gmail.com>
2026-02-09 15:25:13 +0530

    Updated Mongo routes

--- MongoDB/mongo-with-mongoose/index.js
+++ MongoDB/mongo-with-mongoose/index.js
@@ -1,21 +1,47 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import Student from './models/Student.js'
+import  User  from './models/User.js'
 
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/")
+mongoose.connect("mongodb://localhost:27017/test")
 .then(()=> console.log("MongoDB connected (with schema)"))
+.catch(err => console.log(err));
 
-app.post('/student', async (req,res)=> {
+//Create
+app.post('/users', async (req,res) => {
     try {
-        const student = await Student.create(req.body);
-        res.send(student);
+        const user = await User.create(req.body)
+        res.status(201).json(user);
     } catch (err) {
-        res.status(400).send(err.message);
+        res.status(400).json({error : err.message});
     }
 });
+
+app.get("/users", async (req,res)=> {
+    const users = await User.find();
+    res.json(users);
+})
+
+app.get('/users/:id', async (req,res)=> {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+})
+
+app.put("/users/:id", async(req,res) => {
+    const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new:true }
+    );
+    res.json(updatedUser)
+})
+
+app.delete('/users/:id', async (req,res)=> {
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.json(user);
+})
 
 app.listen(3000, ()=> console.log("Server is running"))
