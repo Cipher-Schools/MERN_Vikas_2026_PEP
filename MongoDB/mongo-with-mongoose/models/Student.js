575b8853951b0f6a4e3bb3d294aa287ae08f51be
vikas-mhjn <vk5008953@gmail.com>
2026-02-09 10:37:46 +0530

    mongodb

---
+++ MongoDB/mongo-with-mongoose/models/Student.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        min: 1
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+});
+
+export default mongoose.model("Student", studentSchema);
+
