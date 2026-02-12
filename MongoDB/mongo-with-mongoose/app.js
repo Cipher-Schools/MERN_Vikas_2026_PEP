186b9a4497dcc5e67509624d8483b0a1080da939
vikas-mhjn <vk5008953@gmail.com>
2026-02-12 11:41:56 +0530

    your message

---
+++ MongoDB/mongo-with-mongoose/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', ()=> {});
+app.post('/users', ()=> {});
+app.get('/users/:id', ()=> {});
+app.put('/users/:id', ()=> {});
+app.delete('/users/:id', ()=> {});
+
+app.get("/products", ()=> {})
+app.get("/products", ()=> {})
+app.get("/products", ()=> {})
+app.get("/products", ()=> {})
+app.get("/products", ()=> {})
