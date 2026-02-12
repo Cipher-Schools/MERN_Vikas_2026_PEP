5a79d3d12e112be6f3544035ea3750ee10f6d785
vikas-mhjn <vk5008953@gmail.com>
2026-02-12 11:18:50 +0530

    pagination

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/shopDB")
+    .then(()=> {console.log("MongoDB connected")});
+
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+app.get("/products", async (req,res) => {
+
+    const page = parseInt(req.query.page) || 1;
+
+    const limit = 2;
+
+    const skip = limit * (page - 1);
+
+    const products = await 
+    Product.find({}, { name : 1, _id: 0 })
+           .sort( {price : -1 })
+           .skip(skip)
+           .limit(limit);
+    res.json({
+        page,
+        data: products
+    });
+});
+
+app.listen(3000, ()=> {
+    console.log("http://localhost:3000");
+})
+ 
